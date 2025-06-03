54e9caaefaae7a980edf0c1eac4b0beaa39b3b4d
Dawid-Czyzewski <dawidek12.04@wp.pl>
2025-06-04 00:53:06 +0200

    Update create.blade.php

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,16 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container py-5">
-    <div class="card shadow-sm">
-        <div class="card-header">
-            <h1 class="h4 mb-0">Dodaj nowe zadanie</h1>
-        </div>
-        <div class="card-body">
-            <form action="{{ route('tasks.store') }}" method="POST">
-                @include('tasks._form')
-            </form>
-        </div>
-    </div>
+<div class="container">
+    <h1>Dodaj zadanie</h1>
+
+    <form action="{{ route('tasks.store') }}" method="POST">
+        @csrf
+        @include('tasks._form', ['task' => null])
+    </form>
 </div>
 @endsection
